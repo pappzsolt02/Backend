0da3f894dbaf98c31ae6848155efddbed6f0d07d
your namepappzsolt02 <papp.zsolt@students.jedlik.eu>
2025-02-18 09:56:55 +0100

    UserInfo model és migrations

---
+++ app/Models/UserInfo.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserInfo extends Model
+{
+    protected $table = 'userInfo';
+
+    protected $fillable = [
+        'UserID',
+        'Height',
+        'Weight',
+    ];
+}
